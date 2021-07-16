c2bc194a46863b9f15c3b8cce0f15c1a1f32ac44
Brachintosh <bracha@MacBook-Pro-de-Bracha.local>
2021-07-16 17:51:56 -0300

    Explicacion.txt

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,9 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  x = 30; y = 50;
+  y > x;
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
